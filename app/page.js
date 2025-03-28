001c116e1177ca75889c18dcc6e4a6119cdb8d0a
midCenturyDude7 <mgriffes@gmail.com>
2025-03-27 22:15:26 -0700

    Initial commit | Project structure/org

---
+++ app/page.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div className="flex px-4 items-center min-h-screen">Hello!</div>
+  );
+}
---
+++ app/test.js
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <div className="flex rounded bg-blue-500 p-4 font-bold text-white hover:bg-blue-600">
+      <h1 className="flex min-h-screen items-center">TEST</h1>
+    </div>
+  );
+}
